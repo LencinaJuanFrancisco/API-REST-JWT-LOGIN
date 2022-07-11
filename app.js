ab20f815c2f4e0b8d0535464998845627d1be30b
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2022-07-11 11:02:13 -0300

    final error de sintaxis

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const PORT = process.env.PORT || 3005
 
 /*para subir a heroku */
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'../client/dist')))
+    app.use(express.static(path.join(__dirname,'../client/build')))
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'../client/build/index.html'))
+        res.sendFile(path.resolve(__dirname,'../client/index.html'))
     })
 }
 /*Bootstrap static files */

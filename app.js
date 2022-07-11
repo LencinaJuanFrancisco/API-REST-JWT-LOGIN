91ab2c6392955bf2f34922f959e34bca43d9866a
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2022-07-11 18:43:22 -0300

    ..

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3005
 /*para subir a heroku */
 if(process.env.NODE_ENV === 'production'){
 
-    app.use(express.static(path.join(__dirname,'../client/build')))
+    app.use(express.static(path.join(__dirname,'../client/dist')))
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'../client/index.html'))
     })

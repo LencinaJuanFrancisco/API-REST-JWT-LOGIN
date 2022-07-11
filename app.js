1965186e6cb9ae3b1fe10479ef57ec12423dbf7b
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2022-07-11 10:59:44 -0300

    final error de sintaxis

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3005
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,'../client/dist')))
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'../client/index.html'))
+        res.sendFile(path.resolve(__dirname,'../client/build/index.html'))
     })
 }
 /*Bootstrap static files */

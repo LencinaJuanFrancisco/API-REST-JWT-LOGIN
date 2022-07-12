8831d937c82585f0a52e51cfc2441f8649d5f5e0
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2022-07-12 19:51:30 -0300

    ..

--- client/src/component/HomeUsers.js
+++ client/src/component/HomeUsers.js
@@ -13,7 +13,7 @@ export default function HomeUsers() {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt ex placeat modi magni quia error alias, adipisci rem similique, at omnis eligendi optio eos harum.
                     </p>
                     <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-4" >
-                    {users &&  users.map(user=>(
+                    {users.lenght > 0 &&  users.map(user=>(
                         <div className="flex flex-col items-center p-8 transition-colors duration-200 transform cursor-pointer group hover:bg-teal-600 rounded-xl" key={user.id}>
                             <img className="object-cover w-32 h-32 rounded-full ring-4 ring-gray-300" src={user.image} alt=""/>
                             

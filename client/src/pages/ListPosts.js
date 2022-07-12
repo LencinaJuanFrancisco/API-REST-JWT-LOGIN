413b28711c90e18f3555ba4f04f764fdba404e7f
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2022-07-12 20:10:48 -0300

    ..

--- client/src/pages/ListPosts.js
+++ client/src/pages/ListPosts.js
@@ -64,7 +64,7 @@ export default function ListPosts() {
             </div>
           )}
           <div className="lg:columns-4 md:columns-3  sm:columns-1 gap-1  mx-auto space-y-3 p-5 pb-5 bg-slate-700 min-h-[1800px] ">
-            {result.length >0 && result.map((post) => (
+            {result && result.map((post) => (
               <PostCard key={post.id} post={post} />
             ))}
           </div>

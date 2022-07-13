2585ad5acfb9078763d3efde1ee81f37982136f1
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2022-07-13 07:30:26 -0300

    ...

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const PORT = process.env.PORT || 3005
 if(process.env.NODE_ENV === 'production'){
 
     app.use(express.static(path.join(__dirname,'client/build')))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client/build/index.html'))
-    })
+    // app.get('*',(req,res)=>{
+    //     res.sendFile(path.resolve(__dirname,'client/build/index.html'))
+    // })
 }
 /*Bootstrap static files */
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))

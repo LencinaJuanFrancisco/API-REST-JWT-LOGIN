3c7d372f6f88fb4eeb40a1e638811e43b44e5142
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2022-07-18 10:53:36 -0300

    error de typo

--- server/libs/cloudinary.js
+++ server/libs/cloudinary.js
@@ -1,17 +1,17 @@
-import {v2 as cloudinary} from 'cloudinary'
+const v2 = require('cloudinary')
 
-cloudinary.config({
+v2.config({  
     cloud_name:"dwtltggn7",
     api_key:"616388625615248",
     api_secret:"-QpZaPWI7S5anMDM1RB-HiDLKfg"
 })
 
 export const uploadImage = async filePath =>{
-   return await cloudinary.uploader.upload(filePath,{
+   return await v2.uploader.upload(filePath,{
         folder: 'Blogs UTN'
     })
 }
 
 export const deleteImage = async id=>{
-    return await cloudinary.uploader.destroy(id)
+    return await v2.uploader.destroy(id)
 }
